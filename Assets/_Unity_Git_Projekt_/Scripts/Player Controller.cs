a70b68c2583b2cab2464277df19199a33948b52c
Fredrik <61582421+frpe1@users.noreply.github.com>
2024-10-31 14:32:43 +0100

    Adding Character Controller component
    Also added error handling, and use GetComponent

--- Assets/_Unity_Git_Projekt_/Scripts/Player Controller.cs
+++ Assets/_Unity_Git_Projekt_/Scripts/Player Controller.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private CharacterController cc;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cc = GetComponent<CharacterController>();
+
+        if (cc == null) {
+            Debug.LogError("Character Controller Component is Missing");
+            return;
+        }
     }
 
     // Update is called once per frame
